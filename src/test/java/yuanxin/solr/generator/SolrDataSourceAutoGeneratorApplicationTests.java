11a5a204263fc672a7a1ef118469c0962b55f2cd
huyuanxin <huyuanxin1999@outlook.com>
2020-12-11 10:31:52 +0800

    test

---
+++ src/test/java/yuanxin/solr/generator/SolrDataSourceAutoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package yuanxin.solr.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrDataSourceAutoGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
