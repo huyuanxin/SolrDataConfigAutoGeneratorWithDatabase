48d4f9af379f265abc373d28778899ca54558117
huyuanxin <huyuanxin1999@outlook.com>
2020-12-11 10:31:48 +0800

    main

---
+++ src/main/java/yuanxin/solr/generator/SolrDataConfigAutoGeneratorApplication.java
@@ -0,0 +1,14 @@
+package yuanxin.solr.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huyuanxin
+ */
+@SpringBootApplication
+public class SolrDataConfigAutoGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SolrDataConfigAutoGeneratorApplication.class, args);
+    }
+}
