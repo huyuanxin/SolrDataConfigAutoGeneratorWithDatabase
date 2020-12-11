5f8ea2a32064043f35190393a66436ebde16467e
huyuanxin <huyuanxin1999@outlook.com>
2020-12-11 10:30:36 +0800

    mybatis的配置

---
+++ src/main/java/yuanxin/solr/generator/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package yuanxin.solr.generator.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author huyuanxin
+ * @create 2020/12/10 14:49
+ */
+@MapperScan("yuanxin.solr.generator.dao")
+public class MybatisConfig {
+}
