099844ed2a115bcb62b48e283f7400378f1d69da
huyuanxin <huyuanxin1999@outlook.com>
2020-12-11 10:28:32 +0800

    field接口

---
+++ src/main/java/yuanxin/solr/generator/api/FieldService.java
@@ -0,0 +1,21 @@
+package yuanxin.solr.generator.api;
+
+
+import org.springframework.stereotype.Service;
+import yuanxin.solr.generator.entity.Field;
+
+import java.util.List;
+
+/**
+ * @author huyuanxin
+ * @create 2020/12/10 14:16
+ */
+@Service("FieldService")
+public interface FieldService {
+    /**
+     * 返回要生成的Field {@link List<Field>}
+     * @param tableName 表名 {@link String}
+     * @return 需要生成的Field {@link List<Field>}
+     */
+    List<Field> generatorFieldList(String tableName);
+}
